2581770b874ac36ff6e01f03d35f95d9bd38542c
Levy Moreira <jmoreira@microsoft.com>
2020-11-15 17:40:08 +0000

    Formating code

--- index.js
+++
@@ -1,7 +0,0 @@
-// console.log('Hello World from your main file!');
-export * from './validation';
-
-const myFn = () => {
-    console.log('levy');
-}
-export default myFn;
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
 const TerserPlugin = require('terser-webpack-plugin');
-
-
-
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   plugins: [new webpack.ProgressPlugin()],
 
   externals: {
